e4bd35c64df5e1a79c77d701a9834e949077aaea
Sam Rosenberg <samrose4@gmail.com>
2021-10-07 15:16:30 -0700

    new commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,6 +7,7 @@ import {
   View,
   Button,
   ScrollView,
+  Pressable,
 } from "react-native";
 
 export default function App() {
@@ -18,29 +19,39 @@ export default function App() {
   const [text, onChangeText] = useState("");
 
   const addItemToList = () => {
-    setItems([...items, { todo: text }]);
-    onChangeText("");
-  };
-
-  const generateList = items.map((item, index) => (
-    <View key={index}>
-      <Text style={styles.listItem}>{item.todo}</Text>
-    </View>
-  ));
-
-  const handleKeyPress = (e) => {
-    if (e.nativeEvent.key === "Enter") {
-      addItemToList();
+    if (text.length > 0 && isNaN(text)) {
+      setItems([...items, { todo: text }]);
+      onChangeText("");
     }
   };
 
+  const generateList = items.map((item, index) => (
+    <View key={index} style={styles.listItem}>
+      <MyCheckbox number={index} />
+      <View style={styles.textArea}>
+        <Text style={styles.itemText}>{item.todo}</Text>
+      </View>
+    </View>
+  ));
+
+  function MyCheckbox(props) {
+    const number = props.number;
+
+    function onCheckmarkPress() {
+      setItems(items.filter((value, i) => i !== number));
+    }
+    return (
+      <Pressable
+        style={[styles.checkboxBase]}
+        onPress={onCheckmarkPress}
+      ></Pressable>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Todo's</Text>
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={{ alignItems: "center" }}
-      >
+      <Text style={styles.heading}>To Do's</Text>
+      <ScrollView style={styles.scrollView}>
         <View style={styles.innerScroll}>{generateList}</View>
       </ScrollView>
       <TextInput
@@ -50,7 +61,7 @@ export default function App() {
         value={text}
         autoFocus={true}
         placeholder="Type new entry here"
-        onKeyPress={handleKeyPress}
+        onSubmitEditing={addItemToList}
       />
       <Button
         style={styles.button}
@@ -72,6 +83,7 @@ const styles = StyleSheet.create({
     marginBottom: "2rem",
     justifyContent: "flex-start",
     textAlign: "center",
+    color: "gray"
   },
   heading: {
     fontWeight: "bold",
@@ -79,21 +91,7 @@ const styles = StyleSheet.create({
     paddingBottom: "2rem",
     marginTop: "2rem",
     height: "20vh",
-  },
-  listItem: {
-    border: "2px solid red",
-    borderRadius: "10px",
-    padding: "1rem",
-    margin: "1rem",
-    width: "80vw",
-    fontSize: "1.5rem",
-    textAlign: "center",
-    backgroundColor: "white",
-    maxWidth: 400,
-  },
-  button: {
-    borderRadius: 4,
-    color: "red",
+    color: "gray"
   },
   textInput: {
     width: "80vw",
@@ -112,20 +110,108 @@ const styles = StyleSheet.create({
   innerScroll: {
     alignItems: "center",
   },
+  checkboxBase: {
+    width: 24,
+    height: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "darkgray",
+    backgroundColor: "transparent",
+  },
+  listItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "80vw",
+    height: "10vh",
+    maxWidth: 500,
+    border: "2px solid darkgray",
+    padding: 20,
+    borderRadius: 10,
+    fontSize: "1.5rem",
+    marginBottom: "2rem",
+    boxShadow: '3px 3px 8px darkgray',
+  },
+  itemText: {
+    fontSize: "1.5rem",
+    paddingLeft: "2rem",
+    color: "gray"
+  },
+  textArea: {
+    textAlign: "center",
+  },
 });
 
-// function ListItem(props) {
-//   return(
-//   <Text style={styles.listItem}>{props.item.todo}</Text>
-//   )
+// // function ListItem(props) {
+// //   return(
+// //   <Text style={styles.listItem}>{props.item.todo}</Text>
+// //   )
+// // };
+
+// // const generateList = items.map((item, index) => (
+// //   <View key={index}>
+// //       <ListItem
+// //       item = {item}
+// //       />
+// //     </View>
+
+// // )
+// // );
+
+// const [checkboxIndex, setCheckBoxIndex] = useState(null);
+
+// const ref_input = useRef();
+
+//  const [focus, setFocus] = useState(true);
+
+/* <Ionicons name="checkmark" size={24} color="white" /> */
+
+// checkboxChecked: {
+//   backgroundColor: "white",
+// },
+
+// checkboxLabel: {
+//   marginLeft: 8,
+//   fontWeight: 500,
+//   fontSize: 18,
+// },
+
+// const handleKeyPress = (e) => {
+//   if (e.nativeEvent.key === "Enter") {
+//     addItemToList();
+//   }
 // };
 
-// const generateList = items.map((item, index) => (
-//   <View key={index}>
-//       <ListItem
-//       item = {item}
-//       />
-//     </View>
+// onKeyPress={(e) => handleKeyPress(e)}
+// ref={ref_input}
 
-// )
-// );
+// onSubmitEditing={()=>ref_input.current.focus()}
+
+// listItem: {
+//   border: "2px solid red",
+//   borderRadius: "10px",
+//   padding: "1rem",
+//   margin: "1rem",
+//   width: "80vw",
+//   fontSize: "1.5rem",
+//   textAlign: "center",
+//   backgroundColor: "white",
+//   maxWidth: 400,
+// },
+
+// const [checked, onChange] = useState(false);
+
+// onChange(!checked);
+
+// console.log(items.splice(number, 1));
+
+// () => setFocus(true);
+// () => ref_input.current.focus();
+
+// const [focus, setFocus] = useState(true);
+// import { Ionicons } from "@expo/vector-icons";
+
+// contentContainerStyle={{ alignItems: "center" }}  //was in scrollview
+
+// console.log(number);
